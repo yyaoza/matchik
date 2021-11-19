7d4600f8be81c3321cc4099bfb86bfeaaa783be9
wyao <walt.yao@gmail.com>
2021-11-20 03:38:02 +0800

    crunchbase lookup CLI

--- main.py
+++ main.py
@@ -4,38 +4,92 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 import csv
 import json
+import requests
 
-print('Reload CSV to JSON?')
-yesOrNo = input()
 
-orgCSVFilePath = 'organizations.csv'
-jsonFilePath = 'json_org.json'
+# org_CSVFilePath = 'organizations.csv'
+# allOrg_CSVFilePath = 'allCompanies.csv'
+accOrg_CSVFilePath = 'accCompanies.csv'
+# org_JsonFilePath = 'json_org.json'
+# allOrg_JsonFilePath = 'json_accOrg.json'
 
-# read csv
+# read csv to JSON file
 org_data = {}
-if yesOrNo == 'y':
-    print('loading CSV to JSON...')
-    with open(orgCSVFilePath) as CSVFile:
-        CSVReader = csv.DictReader(CSVFile)
-        for rows in CSVReader:
-            walt = rows['name']
-            org_data[walt] = rows
+allOrg_data = {}
+# print('Reload organizations.csv to JSON?')
+# yesOrNo = input()
+# if yesOrNo == 'y':
 
-    with open(jsonFilePath, 'w') as jsonFile:
-        jsonFile.write(json.dumps(org_data, indent=4))
 
-    print('done')
+# print('loading organizations.csv to DICT...')
+# with open(org_CSVFilePath) as CSVFile:
+#     CSVReader = csv.DictReader(CSVFile)
+#     for rows in CSVReader:
+#         chunk = rows['name']
+#         org_data[chunk] = rows
 
-else:
-    print('loading JSON to dict...')
-    with open(jsonFilePath) as jsonFile:
-        org_data = json.load(jsonFile)
+print('loading accCompanies.csv to DICT...')
+with open(accOrg_CSVFilePath, encoding='utf-8-sig') as CSVFile:
+    CSVReader = csv.DictReader(CSVFile)
+    for rows in CSVReader:
+        chunk = rows['name']
+        allOrg_data[chunk] = rows
+
+allOrg_data = {k.lower(): v for k, v in allOrg_data.items()}
+
+# with open(orgJsonFilePath, 'w') as jsonFile:
+#     jsonFile.write(json.dumps(org_data, indent=4))
+
+# print('finding all the blank permalinks in allCompanies.csv...')
+# for rows in allOrg_data:
+#     if allOrg_data[rows]['permalink'] == '#N/A':
+#         print(allOrg_data[rows]['name'])
 
 while 1:
-    print('find company')
-    search_text = input()
-    if search_text in org_data:
-        print(org_data[search_text])
+    print('Search an org off Crunchbase:')
+    orgName = input()
+    if orgName in allOrg_data:
+        print('Found!')
+        URL = ['https://api.crunchbase.com/api/v4/entities/organizations/',
+               allOrg_data[orgName]['permalink'],
+               '?card_ids=founders,raised_funding_rounds',
+               '&field_ids=founded_on',
+               '&user_key=c4564ba5932862bc86b28ae5e4397cf7']
+        URL = ''.join(URL)
+        print(URL)
+        r = requests.get(url=URL)
+        print(r.json())
+
+    else:
+        print('Nope!')
+
+print('done')
+
+# else:
+#     print('loading JSON to dict...')
+#     with open(orgJsonFilePath) as jsonFile:
+#         org_data = json.load(jsonFile)
+
+# print('Reload accCompanies.csv to JSON?')
+# yesOrNo = input()
+# accOrg_data = {}
+# if yesOrNo == 'y':
+#     print('loading CSV to JSON...')
+#     with open(accOrgCSVFilePath, encoding='utf-8-sig') as CSVFile:
+#         CSVReader = csv.DictReader(CSVFile)
+#         for rows in CSVReader:
+#             chunk = rows['name']
+#             accOrg_data[chunk] = rows
+#
+#     with open(accOrgJsonFilePath, 'w') as jsonFile:
+#         jsonFile.write(json.dumps(accOrg_data, indent=4))
+#
+#     print('done')
+#
+# else:
+#     print('loading JSON to dict...')
+#     with open(accOrgJsonFilePath) as jsonFile:
+#         accOrg_data = json.load(jsonFile)
 
 
 app = Flask(__name__)
